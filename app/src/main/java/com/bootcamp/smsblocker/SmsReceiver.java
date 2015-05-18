a070900756a4a029e0d5e146fbfe3efe08353047
hackspot07 <pyadav.it@outlook.com>
2015-05-18 12:01:37 +0530

    sender name and message toasted

--- app/src/main/java/com/bootcamp/smsblocker/SmsReceiver.java
+++ app/src/main/java/com/bootcamp/smsblocker/SmsReceiver.java
@@ -3,6 +3,9 @@ package com.bootcamp.smsblocker;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.gsm.SmsMessage;
+import android.util.Log;
 import android.widget.Toast;
 
 public class SmsReceiver extends BroadcastReceiver{
@@ -10,7 +13,31 @@ public class SmsReceiver extends BroadcastReceiver{
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        Toast.makeText(context,"SmS Recieved",Toast.LENGTH_SHORT).show();
+        final Bundle bundle = intent.getExtras();
+
+        try {
+
+            if (bundle != null) {
+
+                final Object[] pdusObj = (Object[]) bundle.get("pdus");
+
+                for (int i = 0; i < pdusObj.length; i++) {
+                    
+                    SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
+                    String phoneNumber = currentMessage.getDisplayOriginatingAddress();
+
+                    String senderNum = phoneNumber;
+                    String message = currentMessage.getDisplayMessageBody();
+
+                    Toast.makeText(context, "senderNum: "+ senderNum + ", message: " + message, Toast.LENGTH_SHORT).show();
+
+                }
+            }
+
+        } catch (Exception e) {
+            Log.e("SmsReceiver", "Exception smsReceiver" + e);
+
+        }
 
     }
 
